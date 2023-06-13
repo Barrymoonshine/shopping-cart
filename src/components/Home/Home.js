c17a1adc6f1bd7b0195621273c6a939b9fb43260
Nick Sheward <nickshwrd@gmail.com>
2023-06-13 09:09:46 +0100

    Added more content to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Welcome to FakeStore</h1>
+      <h3>We specialise in being the best fake store possible!</h3>
+      <p>
+        Buy our fake products, and receive nothing in return for your hard
+        earned money - disappointment guaranteed!
+      </p>
+    </div>
+  );
 };
 
 export default Home;

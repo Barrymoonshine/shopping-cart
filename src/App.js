d7bfb99f13e6a35e791ba4919d5f01b41118c7e2
Nick Sheward <nickshwrd@gmail.com>
2023-06-14 11:08:31 +0100

    Styling applied to header and icons imported

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ProductsContainer from './components/ProductsContainer/ProductsContainer'
 import Cart from './components/Cart/Cart';
 import NotFound from './components/NotFound/NotFound';
 import { useState } from 'react';
+import homeIcon from './images/home-icon.png';
+import cheeseIcon from './images/cheese-icon.png';
+import cartIcon from './images/cart-icon.png';
 
 const App = () => {
   const [cart, setCart] = useState([]);
@@ -13,11 +16,6 @@ const App = () => {
   const addToCart = (productNameInput, quantityInput, priceInput) => {
     const priceInteger = parseInt(priceInput.slice(1));
     const cost = quantityInput * priceInteger;
-    console.log(
-      `quantityInput is string?: ${
-        typeof quantityInput === 'string'
-      } priceInteger is string?:${typeof priceInteger === 'string'}`
-    );
     const newProduct = {
       productName: productNameInput,
       quantity: quantityInput,
@@ -30,22 +28,39 @@ const App = () => {
 
   return (
     <>
-      <div>Fake Store</div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to='./'>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to='./components/ProductsContainer/ProductsContainer'>
-              Products
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='./components/Cart/Cart'>Cart</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className='header'>
+        <div className='left-header'>
+          <div className='shop-title'>The Smelly Cheese Co</div>
+        </div>
+        <nav>
+          <ul className='right-nav'>
+            <li>
+              <NavLink to='./'>
+                <div className='nav-links'>
+                  <img className='nav-icons' src={homeIcon} alt='Home' />
+                  Home
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='./components/ProductsContainer/ProductsContainer'>
+                <div className='nav-links'>
+                  <img className='nav-icons' src={cheeseIcon} alt='Products' />
+                  Products
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='./components/Cart/Cart'>
+                <div className='nav-links'>
+                  <img className='nav-icons' src={cartIcon} alt='Cart' />
+                  Cart
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route
